8f57637b3152cb4bac97c125180b6548fc7cf0e6
sanskar <sanskar@novecta.in>
2026-03-23 11:31:17 +0530

    trust proxy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 require('./config/env');
 const app = require('./app');
 const { port } = require('./config/env');
+app.set('trust proxy', 1);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
